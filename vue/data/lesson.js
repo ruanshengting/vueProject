a193826a8e11585c1b7bc55c44bc88452ab559ce
ava <869161652@qq.com>
2022-05-25 15:35:51 +0800

    add all

--- vue/data/lesson.js
+++ vue/data/lesson.js
@@ -1,6 +1,6 @@
 export default [
-  { id: '1', title: 'ava1', isDelete: false, price: 99, comments: 'Hey' },
-  { id: '2', title: 'ava2', isDelete: true, price: 992, comments: 'Hey2' },
-  { id: '3', title: 'ava3', isDelete: false, price: 992, comments: 'Hey3' },
-  { id: '4', title: 'ava4', isDelete: false, price: 992, comments: 'Hey3' },
+  { id: '1', title: 'ava1', isDelete: false, price: 99, comments: 9 },
+  { id: '2', title: 'ava2', isDelete: true, price: 992, comments: 1 },
+  { id: '3', title: 'ava3', isDelete: false, price: 9, comments: 8 },
+  { id: '4', title: 'ava4', isDelete: false, price: 192, comments: 3 },
 ]
--- vue/src/components/LessonFor.vue
+++ vue/src/components/LessonFor.vue
@@ -1,6 +1,6 @@
 <template>
-  <button>价格</button>
-  <button>评论数</button>
+  <button @click="orderBy = 'price'">价格</button>
+  <button @click="orderBy = 'comments'">评论数</button>
   <template v-for="lesson of lessonList" :key="lesson.id">
     <div>{{ lesson.title }}</div>
   </template>
@@ -10,11 +10,13 @@
 import lessons from '../../data/lesson'
 export default {
   data() {
-    return { lessons }
+    return { lessons, orderBy: 'price' }
   },
   computed: {
     lessonList() {
-      return this.lessons
+      return this.lessons.sort((a, b) => {
+        return a[this.orderBy] - b[this.orderBy]
+      })
     },
   },
 }
