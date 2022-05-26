8a34e6a7789e04d96900f10365a4726b77b7083d
ava <869161652@qq.com>
2022-05-26 21:11:05 +0800

    add all

---
+++ props/src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <hr />
+    基本类型是单向流传值。引用类型是双向传值。
+    <hdButton
+      :content="btContent"
+      :disabled="false"
+      type="yes"
+      :testObj="testObj"
+    />
+
+    <hr />
+    <button @click="btContent = '父组件change'">父组件</button>
+    {{ btContent }}
+    <hr />
+    <button @click="testObj.text = '父组件testObj'">父组件testObj</button>
+    {{ testObj }}
+    <!-- <hdButton :content="appcontent" :disabled="false" type="cool" />
+    <hdButton content="Hello" hdTip="提示：" :disabled="true" type="yes" />
+    <hdButton
+      v-bind="{ content: '保存', type: 'yes', style: 'margin-top:20px' }"
+    /> -->
+  </div>
+</template>
+
+<script>
+import hdButton from './components/Button.vue'
+export default {
+  components: { hdButton },
+  data() {
+    return {
+      appcontent: 'hhhhhhhh ',
+      btContent: 'avaTest',
+      testObj: {
+        text: 'test',
+      },
+    }
+  },
+}
+</script>
+
+<style lang="scss" scoped></style>
---
+++ props/src/components/Button.vue
@@ -0,0 +1,71 @@
+<template>
+  <div :class="[type, { disabled }]">
+    <span>{{ hdTip }}</span>
+    显示子组件的content： {{ content }}
+  </div>
+
+  <button @click="content = '子组件的change'">子组件的change</button>
+  <hr />
+  <button @click="text = '子组件改变object'">子组件改变object</button>
+  <span>{{ text }}</span>
+  <!-- <div>{{ arr }}</div> -->
+</template>
+
+<script>
+export default {
+  //props: ['content'], //这个content要用''包起来
+  props: {
+    //Props:用于父组件向子组件传递数据，不要把它当成响应式数据。
+    testObj: {
+      type: Object,
+    },
+    content: {
+      type: String,
+      default: 'Yes',
+      required: true, //一定要有这个值传过来
+    },
+    arr: {
+      type: Array,
+      default() {
+        //数组和对象是引用类型，如果直接default:  相当于所有组件共用一个数据？？引用类型要用函数形式
+        return [1, 2, 3]
+      },
+    },
+    disabled: {
+      type: Boolean,
+      default: false,
+    },
+    type: {
+      type: String,
+      default: 'info',
+      validator(v) {
+        console.log(v)
+        console.log(['yes', 'no', 'info'].includes(v))
+        return ['yes', 'no', 'info'].includes(v)
+      },
+    },
+    hdTip: String,
+  },
+  data() {
+    return {
+      text: this.content, //把这个props变成响应式的初始值
+    }
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+div {
+  background-color: #4b4453;
+  color: #fff;
+  padding: 5px 10px;
+  opacity: 0.6;
+  transition: 1s;
+  &:hover {
+    opacity: 1;
+  }
+  &.disabled {
+    background-color: #c34a36;
+  }
+}
+</style>
