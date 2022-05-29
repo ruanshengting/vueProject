857727ee71ee3fc256cb1be92216f00826f0df40
ava <869161652@qq.com>
2022-05-29 20:50:11 +0800

    在main.js中引入路由，注入到根实例中。

--- vue-crash-2021-copy/src/main.js
+++ vue-crash-2021-copy/src/main.js
@@ -2,3 +2,4 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router' //import router
 createApp(App).use(router).mount('#app')
+// 把路由注入到根实例中，启动路由。这里其实还有一种方法，就像vuex  store 注入到根实例中一样，我们也可以把vueRouter 直接注入到根实例中。在main.js中引入路由，注入到根实例中。
